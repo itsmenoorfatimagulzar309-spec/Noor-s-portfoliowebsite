c2e56d82adaeb1fc22a071af9109324b7638ee2b
itsmenoorfatimagulzar309-spec <itsmenoorfatimagulzar309@gmail.com>
2026-09-05 17:02:41 +0500

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Noor-s-portfoliowebsite/',
   plugins: [react()],
   server: {
     port: 5173,
